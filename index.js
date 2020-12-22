36a161f608c457284e704e1f9b86fe211f8291cf
Maciej Sznurawa <msznurawa@gmail.com>
2020-12-22 11:08:58 +0100

    Add sticky bar & fix some styling issues

---
+++ index.js
@@ -0,0 +1,13 @@
+// "use strict";
+
+// const stickyBar = document.getElementById("sticky-bar");
+
+// const getWindowHeight = (top) => {
+//   let viewportHeight = window.innerHeight;
+//   console.log(stickyBar.style);
+//   stickyBar.style.top = viewportHeight + "px";
+// };
+
+// getWindowHeight();
+
+// window.addEventListener("resize", getWindowHeight);

b268aaa914f8daceca7e9074655e3ed087b5fe4e
Maciej Sznurawa <msznurawa@gmail.com>
2020-12-28 11:58:10 +0100

    Change menu behaviour

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-'use strict';
+"use strict";
 
 /* BASE FUNCTIONS */
 const click = (target, callback) => {
-  target.addEventListener('click', (e) => {
-    return callback(e)
+  target.addEventListener("click", (e) => {
+    return callback(e);
   });
 };
 
 /* BURGER MENU */
-const hamburgerButton = document.querySelector('.hamburger-menu');
-const mainMenu = document.querySelector('.menu-wrapper');
+const hamburgerButton = document.querySelector(".hamburger");
+const mainMenu = document.querySelector(".menu-wrapper");
 
 const showMainMenu = () => {
-  mainMenu.classList.toggle('visible');
+  mainMenu.classList.toggle("visible");
 };
 
 const hamburgerActive = () => {
-  hamburgerButton.classList.toggle('hamburger-active');
+  hamburgerButton.classList.toggle("hamburger--active");
 };
 
-hamburgerButton.addEventListener('click', (e) => {
+hamburgerButton.addEventListener("click", (e) => {
   showMainMenu();
   hamburgerActive();
 });
 
-const menuImageContainer = document.querySelector('.menu-image-container');
+const menuImageContainer = document.querySelector(".menu-image-container");
 
 const showMenuImage = () => {
-  menuImageContainer.classList.add('visible');
+  menuImageContainer.classList.add("visible");
 };
 
 const hideMenuImage = () => {
-  menuImageContainer.classList.remove('visible');
+  menuImageContainer.classList.remove("visible");
 };
 
-const menuItems = document.querySelectorAll('.menu__item--link');
-
+const menuItems = document.querySelectorAll(".menu__item--link");
 
 menuItems.forEach((menuItem) => {
-  menuItem.addEventListener('click', (e) => {
+  menuItem.addEventListener("click", (e) => {
     showMainMenu();
     hamburgerActive();
   });
-  menuItem.addEventListener('mouseenter', (e) => {
+  menuItem.addEventListener("mouseenter", (e) => {
     showMenuImage();
   });
-  menuItem.addEventListener('mouseleave', (e) => {
+  menuItem.addEventListener("mouseleave", (e) => {
     hideMenuImage();
-  })
+  });
 });
